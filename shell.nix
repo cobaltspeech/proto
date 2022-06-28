b4788915fe22b82714be73a254c26eb6cb857d05
Alok Parlikar <alok@cobaltspeech.com>
2022-06-28 10:15:35 +0530

    use upstream grpc-gateway package

--- shell.nix
+++ shell.nix
@@ -3,31 +3,13 @@
 { pkgs ? import (fetchTarball
   "https://github.com/NixOS/nixpkgs/archive/nixpkgs-unstable.tar.gz") { } }:
 
-let
-  protoc-gen-grpc-gateway = pkgs.buildGoModule rec {
-    pname = "protoc-gen-grpc-gateway";
-    version = "2.10.3";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "grpc-ecosystem";
-      repo = "grpc-gateway";
-      rev = "v${version}";
-      sha256 = "sha256-4/iE+sK+ZbG6194i8E1ZHla/7C9blKGRHwM7iX7nvXU=";
-    };
-
-    vendorSha256 = "sha256-FhiTU9VmDZNCPBWrmCqmQo/kPdDe8Da1T2E06CVN2kw=";
-
-    subPackages = [ "protoc-gen-grpc-gateway" ];
-
-  };
-
-in pkgs.mkShell {
+pkgs.mkShell {
   buildInputs = with pkgs; [
     buf
     grpc
+    grpc-gateway
     protobuf
     protoc-gen-go
     protoc-gen-go-grpc
-    protoc-gen-grpc-gateway
   ];
 }
